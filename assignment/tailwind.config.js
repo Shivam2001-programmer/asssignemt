a8671d99f1b162ff1aa142bbc6a558556b1db74b
Shivam Mishra <75406313+Shivam2001-programmer@users.noreply.github.com>
2024-04-08 14:21:20 +0530

    Add files via upload
    update changes

---
+++ assignment/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        bg: "#390745",
+        error: "#8a0707",
+      },
+    },
+  },
+  plugins: [],
+};
